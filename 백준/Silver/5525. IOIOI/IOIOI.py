e4f7ffe12ed273c38355cc606f4a744b260baa30
Kangwoo, Lee <rain5191@naver.com>
2023-10-29 16:45:06 +0900

    [Silver I] Title: IOIOI, Time: 352 ms, Memory: 32368 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+N=int(input())
+M=int(input())
+S=input()
+cnt=0
+i=0
+res=0
+while (i<M-1):
+    if (S[i:i+3] == "IOI"):
+        i += 2
+        cnt += 1
+        if (cnt==N):
+            res +=1
+            cnt -= 1
+    else:
+        i += 1
+        cnt =0
+print(res)

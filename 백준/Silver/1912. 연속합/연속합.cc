fbde6de131fe1cd9b2973c1aece6b65495464f45
Kangwoo, Lee <rain5191@naver.com>
2023-11-07 11:48:25 +0900

    [Silver II] Title: 연속합, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -9,15 +9,15 @@ int N, ans;
 vector<int> vec(100004,0);
 vector<int> dp(100004,0);
 void solve(){
-    int mx = *max_element(vec.begin()+1,vec.begin()+N+1);
-    if(mx <0){
+    int mx = *max_element(vec.begin()+1,vec.begin()+N+1);   //입력받은 배열에 최대값
+    if(mx <0){  // 음수라면
         ans = mx;
     }
-    else{
+    else{   // 양수라면 최대 누적합을 찾는다.
         for(int i=1;i<=N;i++){
             dp[i] = max(dp[i-1]+vec[i],0);
         }
-        ans = *max_element(dp.begin()+1,dp.begin()+N+1);
+        ans = *max_element(dp.begin()+1,dp.begin()+N+1);    //dp 배열중 최대 값을 찾는다.
     }
 
 }

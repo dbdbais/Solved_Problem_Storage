48ef74dc94aa34b7dbfeaf2db4f9c3e68aab8bd1
Kangwoo, Lee <rain5191@naver.com>
2024-01-05 21:24:57 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 10 ms, Memory: 13,548 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.cpp
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.cpp
@@ -1,63 +1,56 @@
 //
-// Created by newbi on 2023-10-27.
+// Created by newbi on 2024-01-05.
 //
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include <string>
-
+#include <iostream>
+#include <algorithm>
+#include <cstring>
 using namespace std;
-int T,N,high,ans;
-vector<int> std_cnt(1004,0);
+int T,N,ans;
+string st;
+int cnt[101];   //카운팅 배열
 
-void split(string input,string delim){
-    string token = "";
-    int conv;
-    long long pos =0;
-    while((pos = input.find(delim)) != string::npos){
-        token = input.substr(0,pos);
-        conv = stoi(token);
-        if(high < conv) high =conv;
-        std_cnt[conv]++;
-        input.erase(0,pos+delim.length());
+void split(string s, string delim){
+    int pos = 0;
+    string token="";
+    while((pos = s.find(delim)) != string::npos){
+        token = s.substr(0,pos);
+        int tmp = stoi(token);  //정수 변환
+        cnt[tmp]++; //카운팅
+        s.erase(0,pos+delim.length());
     }
 }
 
-
-void solve(){
-    auto mx = max_element(std_cnt.begin(),std_cnt.end());
-    for(int i=high;i>=0;i--){
-        if(std_cnt[i] == *mx){
-            ans = i;
-            break;
-        }
-    }
+void fastIO(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 }
+
 void reset(){
-    high = 0;
-    ans =0;
-    fill(std_cnt.begin(),std_cnt.end(),0);
+    memset(cnt,0,sizeof(cnt));
+    ans = 0;
+    st = "";
 }
-
 
 int main(){
-    int idx=0;
-    string ipt;
 
-    cin >> T;
-    while(T){
-        idx++;
+    fastIO();
+
+    cin >> T ;
+    for(int idx =1;idx<=T;idx++){
         cin >> N;
-        string buffer;
-        getline(cin,buffer);    //버퍼 해결
-
-        getline(cin,ipt);
-        split(ipt," ");
-        solve();
-        cout <<"#"<<idx<<" "<<ans<<'\n';
+        cin.ignore();
+        getline(cin,st);
+        split(st," ");
+        int mx = *max_element(cnt,cnt+100); //최대 값
+        for(int j = 100;j>=0;j--){
+            if(cnt[j] == mx){
+                ans = j;
+                break;
+            }
+        }
+        cout << "#"<<idx<<" "<<ans<<'\n';
         reset();
-        T--;
     }
-
     return 0;
 }

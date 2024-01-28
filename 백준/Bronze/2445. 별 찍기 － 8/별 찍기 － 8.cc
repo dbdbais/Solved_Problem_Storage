c278d0f80094790220da0a3e85840ae83a9228c6
Kangwoo, Lee <rain5191@naver.com>
2024-01-28 19:07:04 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,38 @@
+//
+// Created by newbi on 2024-01-23.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout <<"*";
+        }
+        for(int k= 0;k <2*(n-i);k++){
+        cout <<" ";
+        }
+        for(int j=0;j<i;j++){
+            cout <<"*";
+        }
+        cout <<'\n';
+    }
+    for(int i=n-1;i>0;i--){
+        for(int j=0;j<i;j++){
+            cout <<"*";
+        }
+        for(int k= 0;k <2*(n-i);k++){
+            cout <<" ";
+        }
+
+        for(int j=0;j<i;j++){
+            cout <<"*";
+        }
+        cout <<'\n';
+    }
+
+    return 0;
+}

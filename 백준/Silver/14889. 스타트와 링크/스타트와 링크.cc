0bbde89e1db2035a0c76d400b3a280606b5899f6
Kangwoo, Lee <rain5191@naver.com>
2024-02-13 22:30:37 +0900

    [Silver I] Title: 스타트와 링크, Time: 128 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,72 +1,70 @@
 //
-// Created by newbi on 2023-10-13.
+// Created by newbi on 2024-02-13.
 //
-#include <iostream>
-#include <vector>
+
+#include<iostream>
+#include<vector>
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define endl '\n'
 
 using namespace std;
-int N,K,ans = 1e8;
-bool selected[22];
-vector<vector<int>> vec (24,vector<int>(24,0));
-
-void combi(int idx,vector<int> v){  //조합
-    if(v.size() == K){  //nCk
-        vector<int> start;
-        vector<int> link;
-
-        for(int i=0;i<N;i++){
-            if(selected[i]){    //선택된 K개 스타트
-                start.push_back(i);
-            }
-            else{       //선택되지 않는 n-k개는 링크
-                link.push_back(i);
-            }
-        }
-        int asum =0;
-        int bsum =0;
-        for(int i=0;i<K;i++){
-            for(int j=0;j<K;j++){       //모든 경우의 수 조합해 누적합 구한다.
-                asum += vec[start[i]][start[j]];
-                bsum += vec[link[i]][link[j]];
-            }
-        }
-        ans = min(ans,abs(asum-bsum));  //결과 값 업데이트
-
-        return;
-    }
-    for(int i=idx+1;i<N;i++){
-        v.push_back(i);
-        selected[i] = true;
-
-        combi(i,v);
-
-        v.pop_back();
-        selected[i] = false;
-    }
-    return;
-}
-
+int N,ans= 1e9;
+int arr[21][21];
 void fastIO(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 }
 
+void combi(int start,vector<int> v){
+    if(v.size() == N/2){
+        vector<int> _link;
+        For(i,1,N+1){
+            bool found =false;
+            for(auto elem : v){
+                if(elem == i){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) _link.push_back(i);
+        }
+        int ssum =0;
+        for(int i=0;i<v.size();i++){
+            for(int j=i+1;j<v.size();j++){
+                ssum += arr[v[i]][v[j]];
+                ssum += arr[v[j]][v[i]];
+            }
+        }
+        int lsum =0;
+        for(int i=0;i<_link.size();i++){
+            for(int j=i+1;j<_link.size();j++){
+                lsum += arr[_link[i]][_link[j]];
+                lsum += arr[_link[j]][_link[i]];
+            }
+        }
+        if(ans > abs(ssum-lsum)) ans = abs(ssum-lsum);
+        return;
+    }
+    for(int i= start;i<=N;i++){
+        v.push_back(i);
+        combi(i+1,v);
+        v.pop_back();
+    }
+}
+
 int main(){
     fastIO();
-
     cin >> N;
-    K = N/2;
-
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            cin >> vec[i][j];
+    For(i,1,N+1){
+        For(j,1,N+1){
+            cin >> arr[i][j];
         }
     }
     vector<int>v;
-    combi(-1,v);    //  nCk개의 조합 생성
+    combi(1,v);
 
-    cout << ans <<'\n';
+    cout << ans << endl;
 
     return 0;
 }

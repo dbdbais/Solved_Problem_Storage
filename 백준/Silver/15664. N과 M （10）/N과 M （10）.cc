62dc2545d4d1294db344237dafc9c7e699ec4ac2
Kangwoo, Lee <rain5191@naver.com>
2024-04-06 21:56:30 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
+#include <cstring>
 #define endl '\n'
 #define fastIO() ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 #define For(i,a,b) for(int i=a;i<b;i++)
@@ -13,22 +13,24 @@ using namespace std;
 
 int N,M,tmp;
 int input[10];
-set<vector<int>> ans;
 vector<int> vec;
 
 void combi(int start,int depth){
 
     if(depth == M){
-        vector<int>v;
-        for(int i=0;i<M;i++){
-            v.push_back(input[i]);
+        For(i,0,M){
+            cout << input[i] <<" ";
         }
-        ans.insert(v);
+        cout << endl;
         return;
     }
+    bool visited[10001];
+    memset(visited,false,sizeof(visited));
 
     for(int i=start+1;i<vec.size();i++){
+        if(visited[vec[i]]) continue;   //같은 레벨에서 방문한게 있다면
         input[depth] = vec[i];
+        visited[vec[i]] = true;
         combi(i,depth+1);
     }
 
@@ -45,21 +47,6 @@ int main(){
     sort(vec.begin(),vec.end());
 
     combi(-1,0);
-
-    for(auto elem : ans){
-        for(auto e : elem){
-            cout << e <<" ";
-        }
-        cout << endl;
-    }
-    /*
-    for(auto st : ans){
-        for(auto c : st){
-            cout << c <<" ";
-        }
-        cout << endl;
-    }
-     */
 
     return 0;
 }

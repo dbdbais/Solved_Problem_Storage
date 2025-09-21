1169a9c915f596e4bd4814945170eb32f9fbb7ad
Kangwoo, Lee <rain5191@naver.com>
2025-09-21 15:56:49 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.42 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,70 +1,107 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    public boolean [] visited;
-    public ArrayList<Integer> arr[];
-    public int[][] gwires;
+    // 하나의 트리 형태로 연결되어 있다.
+    // 하나를 끊어서 현재 전력망 네트워크를 2개로 분할, 송전탑의 개수를 최대한 비슷하게 분할
+    // 송전탑의 개수 n, 전선 정보 wires 매개변수로 주어진다.
     
-    public int BFS(int start){
-        int ret = 0;
+    ArrayList<Integer> [] lst;
+    Stack<Integer> stk;
+    int gn;
+    int [][] gwires;
+    boolean [] visited;
+    
+    public int DFS(int start){
+        visited[start] = true;
+        int ret = 1;
         
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            visited[cur] = true;
-            ret++;
-            //하나 방문하고 증가
-            for(int adj : arr[cur]){
-                if(visited[adj]) continue;
-                q.add(adj);
-            }
+        for(int adj : lst[start]){
+            if (visited[adj]) continue;
+            ret += DFS(adj);
         }
+        
         return ret;
     }
     
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
- 
-        arr = new ArrayList[n+1];
+        gn = n;
+        gwires = wires;
+        lst = new ArrayList[gn+1];
+        stk = new Stack<>();
         
-        for(int i=0;i<=n;i++){
-            arr[i] = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            lst[i] = new ArrayList<>();
         }
         
-        for(int i=0;i<n-1;i++){
-            int left = wires[i][0];
-            int right = wires[i][1];
-      
-            arr[left].add(right);
-            arr[right].add(left);
-            //넣고
-        }
-        
-        for(int i=0;i<n-1;i++){
-            //i에 해당하는 간선 제거
-            visited = new boolean[n+1];
+        for(int i=0;i<wires.length;i++){
             int left = wires[i][0];
             int right = wires[i][1];
             
-            arr[left].remove(Integer.valueOf(right));
-            arr[right].remove(Integer.valueOf(left));
+            lst[left].add(right);
+            lst[right].add(left);
+        }
+        
+        // for(int i=1;i<=n;i++){
+        //     System.out.print(i+" : ");
+        //     for(int elem : lst[i]){
+        //         System.out.print(elem+" ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        for(int i=0;i<wires.length;i++){
+            int left = wires[i][0];
+            int right = wires[i][1];
             
+            Iterator<Integer> it = lst[left].iterator();
             
-            List<Integer> tlst = new ArrayList<>();
-            
-            for(int j=1;j<=n;j++){
-                if(visited[j] == false){
-                    tlst.add(BFS(j));
+            while(it.hasNext()){
+                Integer v = it.next();
+                if (v == right){
+                    it.remove();
+                    break;
                 }
             }
-            answer = Math.min(answer, Math.abs(tlst.get(0)- tlst.get(1)));
+            
+            Iterator<Integer> it2 = lst[right].iterator();
+            
+            while(it2.hasNext()){
+                Integer v = it2.next();
+                if (v == left){
+                    it2.remove();
+                    break;
+                }
+            }
+            
+            // 삭제하고
+            
+            visited = new boolean[n+1];
+            
+            for(int j =1;j<=n;j++){
+                if(visited[j]) continue;
+                int ret = DFS(j);
+                
+                if(stk.empty()){
+                    stk.push(ret);
+                }
+                else{
+                    int stkret = stk.pop();
+                    answer = Math.min(answer,Math.abs(stkret-ret));
+                    
+                }
+            }
+            //탐지
+            
+            lst[left].add(right);
+            lst[right].add(left);
             
             //원복
-            arr[left].add(right);
-            arr[right].add(left);
-            
         }
+        
+        
+        
         
         return answer;
     }
